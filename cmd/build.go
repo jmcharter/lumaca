05961f0ad793ea2ea96bd90eafb7b2e06fafb0de
Jon Charter <jon@chrt.uk>
2024-03-01 00:03:17 +0000

    add cobra cli to the project

---
+++ cmd/build.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/jmcharter/lumaca/builder"
+	"github.com/spf13/cobra"
+)
+
+// buildCmd represents the build command
+var buildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "Builds and compiles the site's source files into static files ready for deployment.",
+	Long:  `Compiles all source content, templates, and static assets into a complete set of optimized, static HTML and CSS and renders an RSS feed.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		builder.Build()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(buildCmd)
+	buildCmd.Flags().Bool("watch", false, "NOT CURRENTLY IMPLEMENTED - Continuously watch source files for changes and rebuild automitically when changes are detected.")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// buildCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// buildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,16 @@ module github.com/jmcharter/lumaca
 go 1.20
 
 require (
-	github.com/BurntSushi/toml v1.3.2 // indirect
-	github.com/adrg/frontmatter v0.2.0 // indirect
-	github.com/gomarkdown/markdown v0.0.0-20231222211730-1d6d20845b47 // indirect
-	github.com/gosimple/slug v1.14.0 // indirect
+	github.com/BurntSushi/toml v1.3.2
+	github.com/adrg/frontmatter v0.2.0
+	github.com/gomarkdown/markdown v0.0.0-20231222211730-1d6d20845b47
+	github.com/gosimple/slug v1.14.0
+	github.com/spf13/cobra v1.8.0
+)
+
+require (
 	github.com/gosimple/unidecode v1.0.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
