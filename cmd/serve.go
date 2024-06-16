191007fd55d0425516de3136c67424fa32ad49d8
Jon Charter <jon@chrt.uk>
2024-06-16 21:09:30 +0100

    Refactor serve command for graceful shutdown

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -16,7 +21,9 @@ var serveCmd = &cobra.Command{
 	Short: "Serve static content for local development",
 	Long:  `Start a server and serve static content for local development, optionally building content as well`,
 	Run: func(cmd *cobra.Command, args []string) {
-		serveStaticContent(portFlag)
+		if err := serveStaticContent(portFlag); err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
@@ -25,13 +32,30 @@ func init() {
 	serveCmd.Flags().IntVarP(&portFlag, "port", "p", 8080, "Port to serve static content on")
 }
 
-func serveStaticContent(port int) {
-	fs := http.FileServer(http.Dir(cfg.Directories.Dist))
-	http.Handle("/", fs)
-	addr := fmt.Sprintf(":%d", port)
-	fmt.Printf("Serving content on http://localhost%s\n", addr)
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+func serveStaticContent(port int) error {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.FileServer(http.Dir(cfg.Directories.Dist)),
 	}
+	go func() {
+		fmt.Printf("Serving content on http://localhost%s\n", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed to start: %v", err)
+		}
+	}()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	fmt.Println("\nShutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		return fmt.Errorf("Failed to gracefully shutdown server: %w", err)
+	}
+	fmt.Println("Server shutdown successfully.")
+
+	return nil
 }
